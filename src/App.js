3f4559be7deb321af0b6940b0ae0d5550dd01957
Napat <napat.wongchr@gmail.com>
2019-11-26 22:35:11 +0700

    initialize with drag and drop functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import heroes from "./heroes.json";
+import "./App.css";
 
 function App() {
+  const [retinuesList, setRetinuesList] = useState([]);
+
+  const handleOnDragStart = heroId => e => {
+    e.dataTransfer.setData("id", heroId);
+  };
+
+  const handleDragOver = e => {
+    e.preventDefault();
+  };
+
+  const handleOnDrop = e => {
+    let droppedHeroId = e.dataTransfer.getData("id");
+    let filteredHero = heroes.data.filter(hero => hero.id === droppedHeroId);
+    setRetinuesList([...retinuesList, ...filteredHero]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Hero-list-section">
+        <h1>Hero list</h1>
+        <div className="Hero-list">
+          {heroes.data.map(hero => {
+            return (
+              <span
+                className="Hero-item"
+                draggable
+                onDragStart={handleOnDragStart(hero.id)}
+              >
+                {hero.name}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        className="Retinues-calculation-section"
+        onDragOver={handleDragOver}
+        onDrop={handleOnDrop}
+      >
+        <h1>Retinues calculation area</h1>
+        <div className="Card-section">
+          {/* Show card from hero list by dragging here */}
+          {retinuesList.map(item => {
+            return <span className="Hero-item">{item.name}</span>;
+          })}
+        </div>
+        <div className="Retinues-summary">
+          <h3>This is summary</h3>
+        </div>
+      </div>
     </div>
   );
 }
